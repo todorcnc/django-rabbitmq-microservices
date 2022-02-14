810da0bc046717a38d255418c466f6e55cce3d26
TodorCNC <todorcnc@gmail.com>
2022-02-14 21:38:05 +0100

    Created frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./App.css";
+
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div classNameName="App">
+      <Nav />
+
+      <div className="container-fluid">
+        <div className="row">
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <h2>Section title</h2>
+            <div className="table-responsive">
+              <table className="table table-striped table-sm">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1,001</td>
+                    <td>random</td>
+                    <td>data</td>
+                    <td>placeholder</td>
+                    <td>text</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
